2dcf42e4ea2e99c52a9ff6b5d808315b4bb62aa8
KoroLion <korolion31@yandex.ru>
2021-04-18 13:14:09 +0300

    feature: checkmate checking and simple task

--- src/index.ts
+++ src/index.ts
@@ -13,14 +13,29 @@ function main(appEl) {
     boardEl.id = 'board';
     appEl.appendChild(boardEl);
 
+    const chess = new Chess('k7/8/4R3/1R6/8/8/1K6/8 w - - 0 1');
+
+    function onDrop(from, to, piece) {
+        const move = chess.move({
+            from, to,
+            promotion: 'q'
+        })
+        if (move === null) {
+            return 'snapback';
+        }
+
+        if (chess.in_checkmate()) {
+            alert('Task solved!');
+        }
+    }
     // @ts-ignore
-    const board = Chessboard('board', { pieceTheme: '/images/chesspieces/wikipedia/{piece}.png' });
-
-    const chess = new Chess();
-    const moves = chess.moves();
-    chess.move(moves[Math.floor(Math.random() * moves.length)]);
-
+    const board = Chessboard('board', {
+        pieceTheme: '/images/chesspieces/wikipedia/{piece}.png',
+        draggable: true,
+        onDrop: onDrop
+    });
     board.position(chess.fen());
+    console.log(chess.game_over())
 
     const infoEl = document.createElement('div');
     infoEl.innerHTML = `<hr>${chess.pgn()}<hr>${chess.fen()}`;
